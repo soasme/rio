2ea0175ad1e81924b0eac0891462a65303bcc6fe
soasme <soasme@gmail.com>
2016-04-07 09:33:59 +0800

    Refactor getting webhooks from conf.

--- rio/blueprints/api_v1/views.py
+++ rio/blueprints/api_v1/views.py
@@ -8,41 +8,57 @@ Implement of rio api v1 view functions.
 
 from flask import jsonify
 from flask import request
-from sqlalchemy.exc import IntegrityError
-from celery.task.http import URL
+from flask import current_app
+
+from celery.result import AsyncResult
 
 from rio.core import celery
-from rio.models import Webhook
-from rio.models import Topic
 from rio.tasks import get_webhook as apply_get_webhook
 from rio.tasks import post_webhook as apply_post_webhook
 
 from .core import bp
 
+AVAILABLE_METHODS = {'GET', 'POST'}
+
+def _load_webhooks(topic):
+    """Load webhooks by topic.
+
+    * Load from application config `RIO_WEBHOOKS`
+    * Load from yaml file `.rio.yml` in cwd/home/etc. If `RIO_HOTRELOAD_WEBHOOKS`
+      is set to True, Rio will try to load file per request.
+    """
+    return (current_app.config.get('RIO_WEBHOOKS') or {}).get(topic)
+
 
 @bp.route('/publish/<topic>', methods=['GET', 'POST'])
 def publish(topic):
     """Publish message to topic."""
-    topic = Topic.query.filter_by(title=topic).first()
-    if not topic:
-        return 'not found', 404
-    payload = dict(request.values)
+    #: detect whether topic is in app config
+    webhooks = _load_webhooks(topic)
+    if not webhooks:
+        return jsonify(tasks=[])
+
     tasks = []
-    for webhook in Webhook.query.filter_by(topic_id=topic.id).yield_per(20):
-        if webhook.method_id == Webhook.Method.GET:
-            res = apply_get_webhook(webhook.url, payload)
-            tasks.append({'task_id': res.id, 'url': webhook.url})
-        elif webhook.method_id == Webhook.Method.POST:
-            res = apply_post_webhook(webhook.url, payload)
-            tasks.append({'task_id': res.id, 'url': webhook.url})
+    payload = dict(request.values)
+    for method, url in webhooks:
+        method = method.upper()
+        if method not in AVAILABLE_METHODS:
+            continue
+        if method == 'GET':
+            runner = apply_get_webhook
+        elif method == 'POST':
+            runner = apply_post_webhook
+        res = runner(url, payload)
+        tasks.append({'task_id': res.id, 'url': url})
     return jsonify(tasks=tasks)
 
 @bp.route('/tasks/<task_id>')
 def get_task(task_id):
-    from celery.result import AsyncResult
+    """Get task information.
+    """
     job = AsyncResult(task_id, app=celery)
     return jsonify(task=dict(
         id=task_id,
-        state=job.state,
-        result=job.result,
+        status=job.status,
+        retval=job.get() if job.successful() else None,
     ))
