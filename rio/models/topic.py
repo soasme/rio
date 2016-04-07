22efbe9bf6ee7519c5402415eb871a96972429d3
soasme <soasme@gmail.com>
2016-04-07 17:57:13 +0800

    rename to slug

--- rio/models/topic.py
+++ rio/models/topic.py
@@ -12,14 +12,14 @@ class Topic(db.Model):
 
     __tablename__ = 'topic'
     __table_args__ = (
-        db.UniqueConstraint('title', name='ux_topic_title'),
+        db.UniqueConstraint('slug', name='ux_topic_slug'),
         db.ForeignKeyConstraint(
             ['project_id'], ['project.id'], ondelete='CASCADE', name='fk_topic_project'
         ),
     )
 
     id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(64), nullable=False)
+    slug = db.Column(db.String(64), nullable=False)
     project_id = db.Column(db.Integer(), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
