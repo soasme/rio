1326af6ec5a0008ee9cadf5dc1cccba8457150a6
soasme <soasme@gmail.com>
2016-04-08 10:40:41 +0800

    Define `require_sender`.

---
+++ rio/blueprints/api_v1/utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+
+from flask import request
+from flask import jsonify
+
+from rio.models  import validate_sender
+
+
+def require_sender(f):
+    """A decorator that protect emit view function is triggered by a trusted sender.
+
+    Currently, Rio only support Basic Authorization.
+    """
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        if not request.authorization:
+            return jsonify({message='unauthorized'}), 401
+
+        username = request.authorization.username
+        password = request.authorization.password
+
+        if not validate_sender(username, password):
+            return jsonify({message='forbidden'}), 403
+    return decorator
