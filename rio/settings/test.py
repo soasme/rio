77a8bc2063157adc7e611ec1bb42a90272df63be
soasme <soasme@gmail.com>
2016-04-08 12:27:43 +0800

    add testing conf.

---
+++ rio/settings/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+DEBUG = True
+TESTING = True
+
+SECRET_KEY = 'no-secret'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/rio-test.db'
+
+CELERY_BROKER_URL = 'sqla+' + SQLALCHEMY_DATABASE_URI
+CELERY_RESULT_BACKEND = 'db+' + SQLALCHEMY_DATABASE_URI
