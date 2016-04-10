42f4d0d4517376bb46d047b70a9e79aef16980a3
soasme <soasme@gmail.com>
2016-04-10 20:52:15 +0800

    add redis cluster options.

--- rio/settings/dev.py
+++ rio/settings/dev.py
@@ -9,9 +9,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/rio.db'
 CELERY_BROKER_URL = 'redis://localhost/2'
 CELERY_RESULT_BACKEND = 'db+' + SQLALCHEMY_DATABASE_URI
 
-REDIS_DEFAULT_CLUSTERS = [
-    {
-        'host': 'localhost',
-        'port': '6379'
-    },
-]
+REDIS_DEFAULT_CLUSTERS = {
+    0: {'host': '127.0.0.1', 'port': 6379},
+}
--- rio/settings/test.py
+++ rio/settings/test.py
@@ -11,9 +11,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/rio-test.db'
 CELERY_BROKER_URL = 'sqla+' + SQLALCHEMY_DATABASE_URI
 CELERY_RESULT_BACKEND = 'db+' + SQLALCHEMY_DATABASE_URI
 
-REDIS_DEFAULT_CLUSTERS = [
-    {
-        'host': 'localhost',
-        'port': 6379,
-    }
-]
+REDIS_DEFAULT_CLUSTERS = {
+    0: {'host': '127.0.0.1', 'port': 6379},
+}
