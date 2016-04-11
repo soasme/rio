a621c24d0d0918bf2c47b5233d001319d83c8e07
soasme <soasme@gmail.com>
2016-04-11 10:16:18 +0800

    define tests for get event api.

--- tests/blueprints/event/test_views.py
+++ tests/blueprints/event/test_views.py
@@ -37,3 +37,35 @@ def test_emit_topic_success(client, project, topic, webhook, sender_basic_token)
     resp = client.get(url, headers=headers)
     assert resp.status_code == 200
     assert json.loads(resp.data)['tasks']
+
+@pytest.mark.xfail
+def test_emit_topic_should_incr_metrics(client):
+    raise NotImplementedError
+
+def test_emit_topic_should_track_failure_webhook(
+        client, project, topic, webhook, sender_basic_token):
+    url = url_for('event.emit_topic', project_slug=project.slug, topic_slug=topic.slug)
+    headers = {'Authorization': 'Basic %s' % sender_basic_token}
+    resp = client.get(url, headers=headers)
+
+    event_id = resp.headers.get('X-RIO-EVENT-ID')
+    url = url_for('event.get_event', project_slug=project.slug, event_id=event_id)
+    resp = client.get(url, headers=headers)
+    data = json.loads(resp.data)
+    assert data['status'] == 'RAN'
+    assert data['payload'] == {}
+    assert data['tasks'][0]['status'] == 'FAILURE'
+    assert data['tasks'][0]['method'] == webhook.method
+    assert data['tasks'][0]['url'] == webhook.url
+
+@pytest.mark.xfail
+def test_emit_topic_should_ignore_success_webhook(
+        client, project, topic, webhook, sender_basic_token):
+    url = url_for('event.emit_topic', project_slug=project.slug, topic_slug=topic.slug)
+    headers = {'Authorization': 'Basic %s' % sender_basic_token}
+    resp = client.get(url, headers=headers)
+
+    event_id = resp.headers.get('X-RIO-EVENT-ID')
+    url = url_for('event.get_event', project_slug=project.slug, event_id=event_id)
+    resp = client.get(url, headers=headers)
+    assert resp.status_code == 404
