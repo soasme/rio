6b1060a176068a1f1fe19598feceb04b800853d1
soasme <soasme@gmail.com>
2016-04-11 17:15:19 +0800

    catch webhook errors.

--- rio/tasks.py
+++ rio/tasks.py
@@ -9,18 +9,21 @@ Implement of rio tasks based on celery.
 from time import time
 
 from celery import chord
+from requests import ConnectionError
 
 from rio.core import celery
 from rio.utils.http import dispatch_webhook_request
+from rio.utils.http import FailureWebhookError
 
 
 
 @celery.task()
 def call_webhook(event, webhook, payload):
+    """Build request from event,webhook,payoad and parse response."""
     started_at = time()
     request = {'url': webhook['url'], 'method': webhook['method']}
     request['headers'] = {
-        'X-RIO-EVENT': 'project=%s,topic=%s' % (
+        'X-RIO-EVENT': 'uuid=%s,project=%s,topic=%s' % (
             str(event['uuid']),
             event['project'],
             event['topic']
@@ -31,29 +34,38 @@ def call_webhook(event, webhook, payload):
     else:
         request['json'] = payload
 
-    content = dispatch_webhook_request(**request)
-    ended_at = time()
+    try:
+        content = dispatch_webhook_request(**request)
+    except (FailureWebhookError, ConnectionError) as exception:
+        # TODO: propagate to sentry if configured.
+        return dict(
+            parent=str(event['uuid']),
+            error=exception.message,
+            started_at=started_at,
+            ended_at=time(),
+        )
     return dict(
-        parent=event['uuid'],
+        parent=str(event['uuid']),
         content=content,
         started_at=started_at,
-        ended_at=ended_at
+        ended_at=time()
     )
 
 
 @celery.task()
 def merge_webhooks_runset(runset):
+    """Make some statistics on the run set.
+    """
     min_started_at = min([w['started_at'] for w in runset])
     max_ended_at = max([w['ended_at'] for w in runset])
     ellapse = max_ended_at - min_started_at
-    return dict(ellapse=ellapse)
-
-
-@celery.task(bind=True)
-def handle_event_exec_failed(self, uuid):
-    result = self.app.AsyncResult(uuid)
-    # TODO: build graph or send to sentry
-    print 'Captured error: ', result.result, result.traceback
+    errors_count = sum(1 for w in runset if 'error' in w)
+    total_count = len(runset)
+    return dict(
+        ellapse=ellapse,
+        errors_count=errors_count,
+        total_count=total_count,
+    )
 
 
 def exec_event(event, webhooks, payload):
@@ -69,8 +81,7 @@ def exec_event(event, webhooks, payload):
         call_webhook.s(event, webhook, payload)
         for webhook in webhooks
     )
-    errback = handle_event_exec_failed.s()
-    callback = merge_webhooks_runset.subtask(link_error=errback)
+    callback = merge_webhooks_runset.s()
 
     call_promise = chord(calls)
     return call_promise(callback)
