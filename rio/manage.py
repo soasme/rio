db41519066ee0d01355d6c9b1692cfdedc8c60c3
soasme <soasme@gmail.com>
2016-04-18 11:53:09 +0800

    expose sync project command.

--- rio/manage.py
+++ rio/manage.py
@@ -6,12 +6,14 @@ from rio.app import create_app as create_flask
 from rio.core import celery
 from rio.core import migrate
 from rio.commands import RunWorkerCommand
+from rio.commands import SyncProjectCommand
 
 app = create_flask()
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('runworker', RunWorkerCommand())
+manager.add_command('syncproject', SyncProjectCommand())
 
 def main():
     manager.run()
