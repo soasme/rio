06ae948e0bbbeccc3d6875cd180c56541d6b7059
soasme <soasme@gmail.com>
2016-04-21 17:25:52 +0800

    add routing reducer.

--- rio/assets/reducers/index.js
+++ rio/assets/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
+import { routerReducer } from 'react-router-redux'
 import projects from './projects'
 
-const rio = combineReducers({
-  projects: projects
-})
+const reducers = {
+  routing: routerReducer,
+  form,
+  projects
+}
+
+const rio = combineReducers(reducers)
 
 export default rio
