f224c806e229d624f4977901b1169cbc382bc5b8
soasme <soasme@gmail.com>
2016-05-04 12:00:45 +0800

    Bump version: 0.3.1 → 0.3.2

--- rio/settings/default.py
+++ rio/settings/default.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-RIO_VERSION = '0.3.1'
+RIO_VERSION = '0.3.2'
 
 CELERY_IMPORTS = [
     'celery.task.http',
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='rio',
-    version='0.3.1',
+    version='0.3.2',
     description="RESTful event dispatcher based on celery.",
     long_description=readme + '\n\n' + history,
     author="Ju Lin",
